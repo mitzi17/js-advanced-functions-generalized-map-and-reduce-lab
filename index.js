00b7401772b5245bacd7bdf419fa9a9d8c4f23d5
Mitzi17 <m_estrada17@live.com>
2021-04-09 22:57:52 -0700

    commit tree 7187402011f4c37e11997fa91d2955977c8610d5

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+
+function map(src, callback) {
+    let r = []
+  
+    for (let i = 0; i < src.length; i++) {
+      let theElement = src[i]
+      r.push(callback(theElement))
+    }
+  
+    return r;
+  }
+  
+  function reduce(src, callback, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = callback(src[i], r)
+    }
+  
+    return r;
+  }
